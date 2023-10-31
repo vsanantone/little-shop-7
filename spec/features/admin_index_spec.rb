46d66d62c0a3fdbfe8bae17ddaad7702aef8f863
Victor Sanchez <vsanantone@gmail.com>
2023-10-31 17:43:35 -0500

    test: add test for links to the Admin Merchants & Invoice

--- spec/features/admin_index_spec.rb
+++ spec/features/admin_index_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe "Admin Dashboard" do
     # Then I see a header indicating that I am on the admin dashboard
     expect(page).to have_content("Welcome to the Admin Dashboard")
   end
+ it "display a link to the Admin Merchants & Invoice Index page" do
+    # 20. Admin Dashboard Links
+    # As an admin,
+    # When I visit the admin dashboard (/admin)
+    visit "/admins" #admin_path
+
+    # Then I see a link to the admin merchants index (/admin/merchants)
+    expect(page).to have_link("Admin Merchant's Index")
+    click_link("Admin Merchant's Index")
+    expect(current_path).to eq("/admins/merchants")
+    expect(page).to have_content("Admin Merchants")
+    # And I see a link to the admin invoices index (/admin/invoices)
+    visit "/admins" #admin_path
+    expect(page).to have_link("Admin Invoice's Index")
+    click_link("Admin Invoice's Index")
+    expect(current_path).to eq("/admins/invoices")
+    expect(page).to have_content("Admin Invoices")
+
+  end
 end
