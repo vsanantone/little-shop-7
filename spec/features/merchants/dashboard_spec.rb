83fe0debf07eb6b032c96043ff30ceef820b4eca
Xander Hendry <vhendry.coding@gmail.com>
2023-11-01 10:51:37 -0400

    Add links for My Items and My Invoices to merchant dashboard, and add rspec test to check for presence of links

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper' 
 
 RSpec.describe "Merchants Dashboard" do 
-  # before(:each) do 
-  #   @merchant_1 = create(:merchant)
-  # end
+  before(:each) do 
+    @merchant_1 = create(:merchant)
+  end
   describe "visiting the merchant dashboard" do 
     it "displays the name of my merchant" do 
-      merchant_1 = create(:merchant)
-      visit "/merchants/#{merchant_1.id}/dashboard"
+      visit "/merchants/#{@merchant_1.id}/dashboard"
 
-      expect(page).to have_content(merchant_1.name)
+      expect(page).to have_content(@merchant_1.name)
+    end
+    it "displays links to my merchant items index and my merchant invoices index." do 
+      visit "/merchants/#{@merchant_1.id}/dashboard"
+      expect(page).to have_link("My Items", :href => "/merchants/#{@merchant_1.id}/items")
+      expect(page).to have_link("My Invoices", :href => "/merchants/#{@merchant_1.id}/invoices")
     end
   end
 end 
