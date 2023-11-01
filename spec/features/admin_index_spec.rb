491dcb234d56b2afdb433720cec4e7f872f2197f
Eric Belongea <eric.belongea@gmail.com>
2023-11-01 14:31:19 -0600

    Add top five customer AR query

--- spec/features/admin_index_spec.rb
+++ spec/features/admin_index_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Admin Dashboard" do
+  before :each do
+    # @customer1 = Customer.create!(first_name: "John", last_name: "Doe")
+    # @invoice1 = Invoice.create!(status: 1, customer_id: "#{@customer1.id}")
+    # @transaction1 = Transaction.create!(credit_card_number: "1234567890", credit_card_expiration_date: "4/27", result: 1, invoice_id: "#{@invoice1.id}")
+  end
   it "displays a header indicating the user is on the admin dashboard" do
   
     # 19. Admin Dashboard
@@ -10,6 +15,7 @@ RSpec.describe "Admin Dashboard" do
     visit "/admin"
     expect(page).to have_content("Welcome to the Admin Dashboard")
   end
+
   it "display a link to the Admin Merchants & Invoice Index page" do
     # 20. Admin Dashboard Links
     # As an admin,
@@ -27,6 +33,13 @@ RSpec.describe "Admin Dashboard" do
     click_link("Admin Invoice's Index")
     expect(current_path).to eq("/admin/invoices")
     expect(page).to have_content("Admin Invoices")
+  end
 
+  it "Dashboard Statistics - Top Customer" do
+    visit "/admin"
+    # require 'pry'; binding.pry
+    
+    expect(page).to have_content("Welcome to the Admin Dashboard")
+    expect(page).to have_content("Top Customers")
   end
 end
