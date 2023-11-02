f4da152d0342b7f8474465d3bd05208bc39790c7
Victor Sanchez <vsanantone@gmail.com>
2023-11-02 11:00:38 -0500

    only have specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
   
   namespace :admin do
-    resources :merchants
+    resources :merchants, only: [:index, :show]
     resources :invoices
     get "", to: "dashboard#index", as: "dashboard"
   end
