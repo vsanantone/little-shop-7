828f0d61a2b22419562a718da40e32ba88fc2d91
Eric Belongea <eric.belongea@gmail.com>
2023-11-02 15:05:03 -0600

    Remake what I already made do to conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :merchants, only: [:index, :show]
-    resources :invoices
+    resources :invoices, only: [:index, :show]
     get "", to: "dashboard#index", as: "dashboard"
   end
 end
---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Admin Invoice Index" do
+  before(:each) do
+
+  end
+
+  it "Has a list of all invoices" do
+    
+  end
+end
