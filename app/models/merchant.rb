7528162dbe5a099085c2d2dc59882c0bc53e0326
Xander Hendry <vhendry.coding@gmail.com>
2023-11-02 10:06:15 -0400

    add #top_5_customers query to merchant model, organizing customers that have completed transactions with the given merchant, and implemented this in the view.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
 
   validates :name, presence: true
   
+  def top_5_customers
+    top_5 = Customer.joins(invoices: :items).where("invoices.status = 1 AND items.merchant_id = #{self.id}").select("count(invoices.id), customers.*").group("customers.id").order("count").limit(5)
+  end
+
 end
