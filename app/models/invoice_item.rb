a5712c8c1ca28febdc649b70a6139b984b7ef6bf
Eric Belongea <eric.belongea@gmail.com>
2023-11-02 09:58:44 -0600

    Make intitial query

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,4 +7,8 @@ class InvoiceItem < ApplicationRecord
   validates :status, presence: true
 
   enum:status, ["pending", "packaged", "shipped"]
+
+  def self.incomplete_invoices
+    InvoiceItem.select("invoice_id").where("status != 2").group("invoice_id").order("invoice_id")
+  end
 end
