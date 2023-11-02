045a0054642916b0790d8a4a6427e2d7213ffdb5
Xander Hendry <vhendry.coding@gmail.com>
2023-11-02 11:35:40 -0400

    Add test and test setup to model/merchant spec ensuring that #top_5_customers works as intended, Add Test to merchants/dashboard spec testing that elements for #top_5_customers are properly displayed on the view, refined top_5_customers query by adding DESC to ordering, removed save_and_open_page from admin_index_spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,12 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
   
   def top_5_customers
-    top_5 = Customer.joins(invoices: :items).where("invoices.status = 1 AND items.merchant_id = #{self.id}").select("count(invoices.id), customers.*").group("customers.id").order("count").limit(5)
+    top_5 = Customer.joins(invoices: :items)
+      .where("invoices.status = 1 AND items.merchant_id = #{self.id}")
+      .select("count(invoices.id), customers.*")
+      .group("customers.id")
+      .order("count DESC")
+      .limit(5)
   end
 
 end
