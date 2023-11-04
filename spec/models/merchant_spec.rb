26011a2a8ec8235c411037e054d95a6dcb33c5f8
Xander Hendry <vhendry.coding@gmail.com>
2023-11-04 18:56:07 -0400

    add spec test to models/merchants testing #invoices works properly

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,56 +11,56 @@ RSpec.describe Merchant, type: :model do
     
     
     @customer_1 = create(:customer)
-    c1_invoice_1 = create(:invoice, status: 1, customer_id: @customer_1.id)
-    c1_invoice_2 = create(:invoice, status: 1, customer_id: @customer_1.id)
-    c1_invoice_3 = create(:invoice, status: 1, customer_id: @customer_1.id)
-    c1_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c1_invoice_1.id, item_id: @item_1.id)
-    c1_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: c1_invoice_2.id, item_id: @item_1.id)
-    c1_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: c1_invoice_3.id, item_id: @item_1.id)
+    @c1_invoice_1 = create(:invoice, status: 1, customer_id: @customer_1.id)
+    @c1_invoice_2 = create(:invoice, status: 1, customer_id: @customer_1.id)
+    @c1_invoice_3 = create(:invoice, status: 1, customer_id: @customer_1.id)
+    c1_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c1_invoice_1.id, item_id: @item_1.id)
+    c1_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: @c1_invoice_2.id, item_id: @item_1.id)
+    c1_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: @c1_invoice_3.id, item_id: @item_1.id)
     
     @customer_2 = create(:customer)
-     c2_invoice_1 = create(:invoice, status: 1, customer_id: @customer_2.id)
-    c2_invoice_2 = create(:invoice, status: 1, customer_id: @customer_2.id)
-    c2_invoice_3 = create(:invoice, status: 1, customer_id: @customer_2.id)
-    c2_invoice_4 = create(:invoice, status: 1, customer_id: @customer_2.id)
-    c2_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c2_invoice_1.id, item_id: @item_1.id)
-    c2_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: c2_invoice_2.id, item_id: @item_1.id)
-    c2_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: c2_invoice_3.id, item_id: @item_1.id)
-    c2_invoice_item_4 = create(:invoice_item, status: 2, invoice_id: c2_invoice_4.id, item_id: @item_1.id)
+    @c2_invoice_1 = create(:invoice, status: 1, customer_id: @customer_2.id)
+    @c2_invoice_2 = create(:invoice, status: 1, customer_id: @customer_2.id)
+    @c2_invoice_3 = create(:invoice, status: 1, customer_id: @customer_2.id)
+    @c2_invoice_4 = create(:invoice, status: 1, customer_id: @customer_2.id)
+    c2_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c2_invoice_1.id, item_id: @item_1.id)
+    c2_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: @c2_invoice_2.id, item_id: @item_1.id)
+    c2_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: @c2_invoice_3.id, item_id: @item_1.id)
+    c2_invoice_item_4 = create(:invoice_item, status: 2, invoice_id: @c2_invoice_4.id, item_id: @item_1.id)
 
     @customer_3 = create(:customer)
-    c3_invoice_1 = create(:invoice, status: 1, customer_id: @customer_3.id)
-    c3_invoice_2 = create(:invoice, status: 1, customer_id: @customer_3.id)
-    c3_invoice_3 = create(:invoice, status: 1, customer_id: @customer_3.id)
-    c3_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c3_invoice_1.id, item_id: @item_1.id)
-    c3_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: c3_invoice_2.id, item_id: @item_1.id)
-    c3_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: c3_invoice_3.id, item_id: @item_1.id)
+    @c3_invoice_1 = create(:invoice, status: 1, customer_id: @customer_3.id)
+    @c3_invoice_2 = create(:invoice, status: 1, customer_id: @customer_3.id)
+    @c3_invoice_3 = create(:invoice, status: 1, customer_id: @customer_3.id)
+    c3_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c3_invoice_1.id, item_id: @item_1.id)
+    c3_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: @c3_invoice_2.id, item_id: @item_1.id)
+    c3_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: @c3_invoice_3.id, item_id: @item_1.id)
     
 
     @customer_4 = create(:customer)
-    c4_invoice_1 = create(:invoice, status: 1, customer_id: @customer_4.id)
-    c4_invoice_2 = create(:invoice, status: 1, customer_id: @customer_4.id)
-    c4_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c4_invoice_1.id, item_id: @item_1.id)
-    c4_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: c4_invoice_2.id, item_id: @item_1.id)
+    @c4_invoice_1 = create(:invoice, status: 1, customer_id: @customer_4.id)
+    @c4_invoice_2 = create(:invoice, status: 1, customer_id: @customer_4.id)
+    c4_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c4_invoice_1.id, item_id: @item_1.id)
+    c4_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: @c4_invoice_2.id, item_id: @item_1.id)
 
     @customer_5 = create(:customer)
-    c5_invoice_1 = create(:invoice, status: 1, customer_id: @customer_5.id)
-    c5_invoice_2 = create(:invoice, status: 1, customer_id: @customer_5.id)
-    c5_invoice_3 = create(:invoice, status: 1, customer_id: @customer_5.id)
-    c5_invoice_4 = create(:invoice, status: 1, customer_id: @customer_5.id)
-    c5_invoice_5 = create(:invoice, status: 1, customer_id: @customer_5.id)
-    c5_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c5_invoice_1.id, item_id: @item_1.id)
-    c5_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: c5_invoice_2.id, item_id: @item_1.id)
-    c5_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: c5_invoice_3.id, item_id: @item_1.id)
-    c5_invoice_item_4 = create(:invoice_item, status: 2, invoice_id: c5_invoice_4.id, item_id: @item_1.id)
-    c5_invoice_item_5 = create(:invoice_item, status: 2, invoice_id: c5_invoice_5.id, item_id: @item_1.id)
+    @c5_invoice_1 = create(:invoice, status: 1, customer_id: @customer_5.id)
+    @c5_invoice_2 = create(:invoice, status: 1, customer_id: @customer_5.id)
+    @c5_invoice_3 = create(:invoice, status: 1, customer_id: @customer_5.id)
+    @c5_invoice_4 = create(:invoice, status: 1, customer_id: @customer_5.id)
+    @c5_invoice_5 = create(:invoice, status: 1, customer_id: @customer_5.id)
+    c5_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c5_invoice_1.id, item_id: @item_1.id)
+    c5_invoice_item_2 = create(:invoice_item, status: 2, invoice_id: @c5_invoice_2.id, item_id: @item_1.id)
+    c5_invoice_item_3 = create(:invoice_item, status: 2, invoice_id: @c5_invoice_3.id, item_id: @item_1.id)
+    c5_invoice_item_4 = create(:invoice_item, status: 2, invoice_id: @c5_invoice_4.id, item_id: @item_1.id)
+    c5_invoice_item_5 = create(:invoice_item, status: 2, invoice_id: @c5_invoice_5.id, item_id: @item_1.id)
 
     @customer_6 = create(:customer)
-    c6_invoice_1 = create(:invoice, status: 1, customer_id: @customer_6.id)
+    @c6_invoice_1 = create(:invoice, status: 1, customer_id: @customer_6.id)
     @c6_invoice_2 = create(:invoice, status: 0, customer_id: @customer_6.id, created_at: "2023-10-19 UTC")
     @c6_invoice_3 = create(:invoice, status: 0, customer_id: @customer_6.id, created_at: "2023-10-20 UTC")
     @c6_invoice_4 = create(:invoice, status: 0, customer_id: @customer_6.id, created_at: "2023-10-21 UTC")
-    c6_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: c6_invoice_1.id, item_id: @item_1.id)
+    c6_invoice_item_1 = create(:invoice_item, status: 2, invoice_id: @c6_invoice_1.id, item_id: @item_1.id)
     c6_invoice_item_2 = create(:invoice_item, status: 0, invoice_id: @c6_invoice_2.id, item_id: @item_4.id)
     c6_invoice_item_3 = create(:invoice_item, status: 1, invoice_id: @c6_invoice_3.id, item_id: @item_3.id)
     c6_invoice_item_4 = create(:invoice_item, status: 0, invoice_id: @c6_invoice_4.id, item_id: @item_2.id)
@@ -86,6 +86,13 @@ RSpec.describe Merchant, type: :model do
     it "returns item names, invoice id's and invoice creation dates for all items that are ready to be shipped, ordered oldest to newest." do 
       ready_to_ship = @merchant.shippable_items
       expect(ready_to_ship.first.name).to eq(@item_4.name)
+    end
+  end
+
+  describe "#invoices" do 
+    it "returns all invoices that have at least 1 item that is sold by a merchant" do 
+      invoices = @merchant.invoices
+      expect([invoices.first.id, invoices.last.id]).to eq([@c1_invoice_1.id, @c6_invoice_4.id])
     end
   end
 end
