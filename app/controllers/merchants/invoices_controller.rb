abe0548b83f08dfa8e305ab85213fc0d380cfeeb
Xander Hendry <vhendry.coding@gmail.com>
2023-11-06 17:29:51 -0500

    refactor to better follow MVC

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -2,12 +2,10 @@ class Merchants::InvoicesController < ApplicationController
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @invoices = @merchant.invoices
   end
+  
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
-    @customer = @invoice.customer
-    @my_items = @invoice.merchant_items(@merchant.id)
   end
 end
