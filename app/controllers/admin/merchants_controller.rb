4be7af2372845851645998290d77549930bc983c
Victor Sanchez <vsanantone@gmail.com>
2023-11-06 19:04:41 -0600

    feat: add the update method and code for the Admin::Merchants Controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,13 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    #find our merchant
+    @merchant = Merchant.find(params[:id])
+    #use the request to update the merchant 
+    @merchant.update(enabled: params[:enabled])
+    #redirect to the admin merchants index
+    redirect_to admin_merchants_path
+  end
 end
