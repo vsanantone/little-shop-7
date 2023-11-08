453ee5b01d86e5b1e3d40e59e53086367b35559d
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-11-07 21:28:17 -0600

    US 12+13 complete

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -122,63 +122,73 @@ RSpec.describe "Merchant Items Index" do
     end
   end
 
-  describe "US 12 - Most Popular Items" do
-    describe "I see the names of the top 5 most popular items by total revenue generate" do
-      describe "each item links to its show page" do
-        it "total revenue generated is also next to each item" do
-          @item_5 = create(:item, merchant: @merchant_1)
-          @item_6 = create(:item, merchant: @merchant_1)
-          @item_7 = create(:item, merchant: @merchant_1)
+  describe "US 12 and 13 data" do
+    before(:each) do
+      @item_5 = create(:item, merchant: @merchant_1)
+      @item_6 = create(:item, merchant: @merchant_1)
+      @item_7 = create(:item, merchant: @merchant_1)
 
-          @customer_1 = create(:customer)
-          @customer_2 = create(:customer)
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
 
-          @invoice_1 = create(:invoice, customer: @customer_1)
-          @invoice_2 = create(:invoice, customer: @customer_1)
-          @invoice_3 = create(:invoice, customer: @customer_2)
-          @invoice_4 = create(:invoice, customer: @customer_2)
+      @invoice_1 = create(:invoice, customer: @customer_1, created_at: "2023-11-07 07:07:07 UTC")
+      @invoice_2 = create(:invoice, customer: @customer_1, created_at: "2023-11-07 07:07:07 UTC")
+      @invoice_3 = create(:invoice, customer: @customer_2, created_at: "2021-07-11 11:11:11 UTC")
+      @invoice_4 = create(:invoice, customer: @customer_2, created_at: "2021-07-11 11:11:11 UTC")
 
-          @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 500, status: 1)
-          @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_1, quantity: 1, unit_price: 1000, status: 1)
-          @invoice_item_3 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 1000, status: 1)
-          @invoice_item_4 = create(:invoice_item, item: @item_3, invoice: @invoice_2, quantity: 2, unit_price: 1500, status: 1)
-          @invoice_item_5 = create(:invoice_item, item: @item_4, invoice: @invoice_2, quantity: 10, unit_price: 40, status: 1)
-          @invoice_item_6 = create(:invoice_item, item: @item_5, invoice: @invoice_1, quantity: 1, unit_price: 1000, status: 1)
-          @invoice_item_7 = create(:invoice_item, item: @item_6, invoice: @invoice_1, quantity: 7, unit_price: 500, status: 1)
-          @invoice_item_8 = create(:invoice_item, item: @item_7, invoice: @invoice_1, quantity: 4, unit_price: 1200, status: 1)
-          @invoice_item_9 = create(:invoice_item, item: @item_7, invoice: @invoice_3, quantity: 10, unit_price: 1200, status: 1)
-          @invoice_item_10 = create(:invoice_item, item: @item_7, invoice: @invoice_4, quantity: 7, unit_price: 1000, status: 1)
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 500, status: 1)
+      @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_1, quantity: 1, unit_price: 1000, status: 1)
+      @invoice_item_3 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 1000, status: 1)
+      @invoice_item_4 = create(:invoice_item, item: @item_3, invoice: @invoice_2, quantity: 2, unit_price: 1500, status: 1)
+      @invoice_item_5 = create(:invoice_item, item: @item_4, invoice: @invoice_2, quantity: 10, unit_price: 40, status: 1)
+      @invoice_item_6 = create(:invoice_item, item: @item_5, invoice: @invoice_1, quantity: 1, unit_price: 1000, status: 1)
+      @invoice_item_7 = create(:invoice_item, item: @item_6, invoice: @invoice_1, quantity: 7, unit_price: 500, status: 1)
+      @invoice_item_8 = create(:invoice_item, item: @item_7, invoice: @invoice_1, quantity: 4, unit_price: 1200, status: 1)
+      @invoice_item_9 = create(:invoice_item, item: @item_7, invoice: @invoice_3, quantity: 10, unit_price: 1200, status: 1)
+      @invoice_item_10 = create(:invoice_item, item: @item_7, invoice: @invoice_4, quantity: 7, unit_price: 1000, status: 1)
+    end
+    describe "US 12 - Most Popular Items" do
+      describe "I see the names of the top 5 most popular items by total revenue generate" do
+        describe "each item links to its show page" do
+          it "total revenue generated is also next to each item" do
+            visit "/merchants/#{@merchant_1.id}/items"
 
-          visit "/merchants/#{@merchant_1.id}/items"
+            expect(page).to have_content("Top 5 Most Popular Items")
 
-          expect(page).to have_content("Top 5 Most Popular Items")
+            within("#popular-items") do
+              expect(page).to have_link(@item_2.name)
+              expect(page).to have_link(@item_3.name)
+              expect(page).to have_link(@item_5.name)
+              expect(page).to have_link(@item_6.name)
+              expect(page).to have_link(@item_7.name)
 
-          within("#popular-items") do
-            expect(page).to have_link(@item_2.name)
-            expect(page).to have_link(@item_3.name)
-            expect(page).to have_link(@item_5.name)
-            expect(page).to have_link(@item_6.name)
-            expect(page).to have_link(@item_7.name)
+              expect(@item_7.name).to appear_before(@item_2.name)
+              expect(@item_2.name).to appear_before(@item_6.name)
+              expect(@item_6.name).to appear_before(@item_3.name)
+              expect(@item_3.name).to appear_before(@item_5.name)
 
-            expect(@item_7.name).to appear_before(@item_2.name)
-            expect(@item_2.name).to appear_before(@item_6.name)
-            expect(@item_6.name).to appear_before(@item_3.name)
-            expect(@item_3.name).to appear_before(@item_5.name)
-
-            expect(page).to have_content("#{@item_7.name} Total revenue: $238.00")
-            expect(page).to have_content("#{@item_2.name} Total revenue: $40.00")
-            expect(page).to have_content("#{@item_6.name} Total revenue: $35.00")
-            expect(page).to have_content("#{@item_3.name} Total revenue: $30.00")
-            expect(page).to have_content("#{@item_5.name} Total revenue: $10.00")
+              expect(page).to have_content("#{@item_7.name} Total revenue: $238.00")
+              expect(page).to have_content("#{@item_2.name} Total revenue: $40.00")
+              expect(page).to have_content("#{@item_6.name} Total revenue: $35.00")
+              expect(page).to have_content("#{@item_3.name} Total revenue: $30.00")
+              expect(page).to have_content("#{@item_5.name} Total revenue: $10.00")
+            end
           end
         end
       end
     end
-  end
 
-  describe "US 13 - Top Items Best Day" do
-    describe "next to each of the 5 most popular items I see the date with the most sales for each item" do
-      it "I see a label “Top selling date for <item name> was <date with most sales>" do
+    describe "US 13 - Top Items Best Day" do
+      describe "next to each of the 5 most popular items I see the date with the most sales for each item" do
+        it "I see a label “Top selling date for <item name> was <date with most sales>" do
+          visit "/merchants/#{@merchant_1.id}/items"
+
+          expect(page).to have_content("Top selling date for #{@item_7.name} was 07/11/2021")
+          expect(page).to have_content("Top selling date for #{@item_2.name} was 11/07/2023")
+          expect(page).to have_content("Top selling date for #{@item_6.name} was 11/07/2023")
+          expect(page).to have_content("Top selling date for #{@item_3.name} was 11/07/2023")
+          expect(page).to have_content("Top selling date for #{@item_5.name} was 11/07/2023")
+        end
       end
     end
   end
